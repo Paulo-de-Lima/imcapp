8266b5cb46d5a53c5b041d93da519af4d68c69c0
Paulo de Lima Barbosa Júnior <241.14.009@EDU.gruposete.local>
2026-02-12 19:51:29 -0300

    Aula12/02/26

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Altura</Text>
-      <TextInput style={styles.campo}></TextInput>
-      <Text style={styles.label}>Peso</Text>
-      <TextInput style={styles.campo}></TextInput>
+      <View style={styles.containerLogo}>
+          <Image style={styles.logo} source={require('./assets/logo-app-imc.png')} />
+      </View>
 
-      <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btntext}>Calcular</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <Text style={styles.label}>Altura</Text>
+        <TextInput style={styles.campo}></TextInput>
+        <Text style={styles.label}>Peso</Text>
+        <TextInput style={styles.campo}></TextInput>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntext}>Calcular</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,28 +26,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#06C',
+  },
+
+  containerLogo: {
+    height: 150,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
   },
-  campo: {
-    backgroundColor: '#999',
-    width: '100%',
-    marginBottom: 20,
+
+  logo: {
+    width: 170,
+    height: 60,
+  },
+
+  form: {
+    backgroundColor: '#fff',
     padding: 30,
+    height: '100%',
+    borderTopEndRadius: 30,
+    borderTopStartRadius: 30,
+  },
+
+  campo: {
+    backgroundColor: '#DDD',
+    width: '100%',
+    height: 70,
+    marginBottom: 20,
+    padding: 20,
     borderRadius: 10,
+    fontSize: 20,
   },
   label: {
     marginBottom: 10,
+    fontSize: 22,
   },
   btn: {
     backgroundColor: '#F90',
     width: '100%',
     padding: 10,
     borderRadius: 10,
+    height: 70,
+    justifyContent: 'center',
   },
   btntext: {
     textAlign: 'center',
+    fontSize: 22,
+    color: '#fff',
   }
 });
